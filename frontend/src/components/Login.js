d3a7f4900c5af79fe7c2c08f38166c5b0a240fd0
jayasravanis <jsirigin@uncc.edu>
2025-04-21 14:10:40 -0400

    Modify login and registration components with form handling and API integration

--- frontend/src/components/Login.js
+++ frontend/src/components/Login.js
@@ -1,16 +1,96 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link, withRouter } from 'react-router-dom';
+import { AuthContext } from '../AuthContext';
 
 class Login extends Component {
-    
-   
+    constructor(props) {
+        super(props);
+        this.state = {
+            Name: '',
+            Password: ''
+        };
+        this.changename = this.changename.bind(this);
+        this.changepwd = this.changepwd.bind(this);
+        this.changeSubmit = this.changeSubmit.bind(this);
+    }
+
+    changename(event) {
+        this.setState({
+            Name: event.target.value
+        });
+    }
+
+    changepwd(event) {
+        this.setState({
+            Password: event.target.value
+        });
+    }
+
+    changeSubmit(event) {
+        event.preventDefault();
+        axios.post('http://localhost:8080/login', {
+            Password: this.state.Password,
+            Name: this.state.Name,
+        }, {
+            headers: { 'Content-Type': 'application/json' }
+        })
+            .then((res) => {
+                if (res.data.data === 1) {
+                    this.props.authContext.login(); // context login
+                    this.props.history.push("/Homes");
+                } else {
+                    alert("Invalid Username or password");
+                    this.props.history.push("/Login");
+                }
+            })
+            .catch((err) => {
+                console.log(err);
+                alert("Invalid Username or password");
+                this.props.history.push("/Login");
+            });
+    }
+
     render() {
         return (
-                <h2 align='center'>
-                    Login Form
-                </h2>
-                
-        )
+            <div style={{ margin: '130px'}}>
+                <div style={{ width: '55%', margin: 'auto' }}>
+                    <br />
+                    <h2 align='center'>Ready for More Pizza? 🔥</h2>
+                    <br />
+                    <form onSubmit={this.changeSubmit}>
+                        <div className='form-group row'>
+                            <label htmlFor="name" className="col-sm-2 col-form-label">Username</label>
+                            <div className='col-sm-9'>
+                                <input type="text" className="form-control" name='Name' id="Name" placeholder="Enter Username" value={this.state.Name} onChange={this.changename} required />
+                            </div>
+                        </div>
+                        <br />
+                        <div className="form-group row">
+                            <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+                            <div className='col-sm-9'>
+                                <input type="password" className="form-control" name='Password' id="Password" placeholder="Enter Password" value={this.state.Password} onChange={this.changepwd} required />
+                            </div>
+                        </div>
+                        <br />
+                        <center>
+                            <button type="submit" className="btn btn-success btn-xs">Login</button>
+                        </center>
+                    </form>
+                    <div>
+                        For New User <Link to='/Register' className="text-success">Registration</Link>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
-export default Login;
+// Inject AuthContext into the class component
+const LoginWithContext = (props) => (
+    <AuthContext.Consumer>
+        {(authContext) => <Login {...props} authContext={authContext} />}
+    </AuthContext.Consumer>
+);
+
+export default withRouter(LoginWithContext);
--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -1,16 +1,117 @@
 import React, { Component } from 'react';
+import axios from 'axios';
+import { Link, withRouter } from 'react-router-dom';
+import { AuthContext } from '../AuthContext';
 
 class Register extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            name: '',
+            mob: '',
+            password: '',
+            email: ''
+        };
+
+        this.changename = this.changename.bind(this);
+        this.changeemail = this.changeemail.bind(this);
+        this.changemob = this.changemob.bind(this);
+        this.changepassword = this.changepassword.bind(this);
+        this.changeSubmit = this.changeSubmit.bind(this);
+    }
+
+    changename(event) {
+        this.setState({ name: event.target.value });
+    }
+
+    changeemail(event) {
+        this.setState({ email: event.target.value });
+    }
+
+    changemob(event) {
+        this.setState({ mob: event.target.value });
+    }
+
+    changepassword(event) {
+        this.setState({ password: event.target.value });
+    }
+
+    changeSubmit(event) {
+        event.preventDefault();
+        axios.post('http://localhost:8080/register', {
+            mob: this.state.mob,
+            name: this.state.name,
+            email: this.state.email,
+            password: this.state.password
+        })
+            .then((res) => {
+                console.log(res);
+                alert("You have successfully registered " + this.state.name);
+                this.props.history.push("/Login");
+            })
+            .catch((err) => {
+                console.log(err);
+                alert("Registration failed");
+            });
+    }
 
     render() {
         return (
-            
-            <h2 align='center'>
-                Hey Foodie, Register Here
-            </h2>
-                    
-        )
+            <div style={{ margin: '130px' }}>
+                <div style={{ width: '55%', margin: 'auto' }}>
+                    <br />
+                    <h2 align='center'>
+                        Join the Pizza Party! 🎉
+                    </h2>
+                    <br />
+                    <form onSubmit={this.changeSubmit}>
+                        <div className='form-group row'>
+                            <label htmlFor="name" className="col-sm-2 col-form-label">Username</label>
+                            <div className='col-sm-9'>
+                                <input type="text" className="form-control" id="name" placeholder="Enter Username" value={this.state.name} onChange={this.changename} required />
+                            </div>
+                        </div>
+                        <br />
+                        <div className='form-group row'>
+                            <label htmlFor="email" className="col-sm-2 col-form-label">Email</label>
+                            <div className='col-sm-9'>
+                                <input type="email" className="form-control" id="email" placeholder="Enter Your Email" value={this.state.email} onChange={this.changeemail} required />
+                            </div>
+                        </div>
+                        <br />
+                        <div className='form-group row'>
+                            <label htmlFor="mobile" className="col-sm-2 col-form-label">Mobile</label>
+                            <div className='col-sm-9'>
+                                <input type="text" className="form-control" id="mobile" placeholder="Enter Mobile Number" value={this.state.mob} onChange={this.changemob} required />
+                            </div>
+                        </div>
+                        <br />
+                        <div className="form-group row">
+                            <label htmlFor="password" className="col-sm-2 col-form-label">Password</label>
+                            <div className='col-sm-9'>
+                                <input type="password" className="form-control" id="password" placeholder="Enter Password" value={this.state.password} onChange={this.changepassword} required />
+                            </div>
+                        </div>
+                        <br />
+                        <center>
+                            <button type="submit" className="btn btn-success btn-xs">Register</button>
+                        </center>
+                    </form>
+                    <div>
+                        Existing User? <Link to='/Login' className="text-success">Login</Link>
+                    </div>
+                </div>
+            </div>
+        );
     }
 }
 
-export default Register;
+// Inject AuthContext into the class component
+const RegisterWithContext = (props) => (
+    <AuthContext.Consumer>
+        {(authContext) => <Register {...props} authContext={authContext} />}
+    </AuthContext.Consumer>
+);
+
+export default withRouter(RegisterWithContext);
